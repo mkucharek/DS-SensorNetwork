4d808314f3021c8c7958a306bf090b24de509969
Maciej Kucharek <s091828@student.dtu.dk>
2012-03-17 20:10:35 +0100

    Initial commit. Created simple project.

---
+++ src/main/java/dk/dtu/distributedsystems/projects/sensornetwork/App.java
@@ -0,0 +1,13 @@
+package dk.dtu.distributedsystems.projects.sensornetwork;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
