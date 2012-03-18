2ef048cb6f96773f68a1e6bd2e82a2ecf58e247d
Wojciech Pawlak <wojciechpawlak@gmail.com>
2012-03-18 19:07:36 +0100

    Implemented first test.

--- admin/src/test/java/dk/dtu/imm/distributedsystems/projects/sensornetwork/admin/AdminTest.java
+++ admin/src/test/java/dk/dtu/imm/distributedsystems/projects/sensornetwork/admin/AdminTest.java
@@ -8,7 +8,7 @@ public class AdminTest {
 
 	@Test
 	public void test() {
-//		fail("Not yet implemented");
+		assertTrue(3>2);
 	}
 
 }
--- sensor/src/test/java/dk/dtu/imm/distributedsystems/projects/sensornetwork/sensor/SensorTest.java
+++ sensor/src/test/java/dk/dtu/imm/distributedsystems/projects/sensornetwork/sensor/SensorTest.java
@@ -8,7 +8,7 @@ public class SensorTest {
 
 	@Test
 	public void test() {
-//		fail("Not yet implemented");
+		assertTrue(3>2);
 	}
 
 }
