dd72538ad4e9ba2709523887215e36aef798d672
Wojciech Pawlak <wojciechpawlak@gmail.com>
2012-03-19 19:33:18 +0100

    Implemented properties for Admin Node. Wrote tests to check Admin properties. Working.

--- admin/src/main/java/dk/dtu/imm/distributedsystems/projects/sensornetwork/admin/Admin.java
+++ admin/src/main/java/dk/dtu/imm/distributedsystems/projects/sensornetwork/admin/Admin.java
@@ -1,13 +1,32 @@
 package dk.dtu.imm.distributedsystems.projects.sensornetwork.admin;
 
+import java.io.*;
+import java.util.Properties;
+
 /**
- * Hello world!
+ * Admin Node for Sensor Network
  *
  */
 public class Admin 
 {
-    public static void main( String[] args )
+	int port;
+    String sinkIP;
+    int sinkPort;
+	
+    public Admin(String s) {
+		Properties properties = new Properties();
+		try {
+		    properties.load(new FileInputStream(s));
+		} catch (IOException e) {
+		}
+		
+		port = Integer.parseInt(properties.getProperty("PORT"));
+		sinkIP = properties.getProperty("SINK_IP");
+		sinkPort = Integer.parseInt(properties.getProperty("SINK_PORT"));
+    }
+    
+	public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+	
     }
 }
--- admin/src/test/java/dk/dtu/imm/distributedsystems/projects/sensornetwork/admin/AdminTest.java
+++ admin/src/test/java/dk/dtu/imm/distributedsystems/projects/sensornetwork/admin/AdminTest.java
@@ -2,17 +2,24 @@ package dk.dtu.imm.distributedsystems.projects.sensornetwork.admin;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class AdminTest {
 
+	private Admin admin;
+	
+	@Before
+	public void setup() {
+		admin = new Admin("C:\\Users\\Wojtek\\Documents\\Git Repositories\\ds-sensornetwork\\admin\\src\\test\\java\\dk\\dtu\\imm\\distributedsystems\\projects\\sensornetwork\\admin\\admin.properties");
+	}
+	
 	@Test
 	public void test() {
-		assertTrue(3>2);
-		
-		assertTrue(5<6);
-		
-		assertTrue(5<7);
+
+		assertEquals(admin.port, 20000);
+		assertEquals(admin.sinkIP, "192.168.1.101");
+		assertEquals(admin.sinkPort, 20101);
 	}
 
 }
