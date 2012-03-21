0406bb385e6fefd66b01bbc2e463c73b36f50a88
Wojciech Pawlak <wojciechpawlak@gmail.com>
2012-03-21 16:53:46 +0100

    Implemented properties for Admin, Sink and Sensor nodes.

--- sensor/src/main/java/dk/dtu/imm/distributedsystems/projects/sensornetwork/sensor/Sensor.java
+++ sensor/src/main/java/dk/dtu/imm/distributedsystems/projects/sensornetwork/sensor/Sensor.java
@@ -1,13 +1,50 @@
 package dk.dtu.imm.distributedsystems.projects.sensornetwork.sensor;
 
+import java.util.Arrays;
+
 /**
- * Hello world!
+ * Sensor Node for Sensor Network
  *
  */
 public class Sensor 
 {
-    public static void main( String[] args )
+	int id;
+	int period;
+	int threshold;
+	
+	int leftPort;
+	int rightPort;
+	
+	int[] leftChannelIDs;
+	String[] leftChannelIPs;
+	int[] leftChannelPorts;
+
+	int[] rightChannelIDs;
+	String[] rightChannelIPs;
+	int[] rightChannelPorts;
+	
+    public Sensor(	int id, int period,	int threshold,	
+					int leftPort, int rightPort,
+					int[] leftChannelIDs, String[] leftChannelIPs, int[] leftChannelPorts,
+					int[] rightChannelIDs, String[] rightChannelIPs, int[] rightChannelPorts) {	
+    	this.id = id;
+    	this.period = period;
+    	this.threshold = threshold;
+    	
+    	this.leftPort = leftPort;
+    	this.rightPort = rightPort;
+    	
+    	this.leftChannelIDs = Arrays.copyOf(leftChannelIDs, leftChannelIDs.length);
+    	this.leftChannelIPs = Arrays.copyOf(leftChannelIPs, leftChannelIPs.length);
+    	this.leftChannelPorts = Arrays.copyOf(leftChannelPorts, leftChannelPorts.length);
+
+    	this.rightChannelIDs = Arrays.copyOf(rightChannelIDs, rightChannelIDs.length);
+    	this.rightChannelIPs = Arrays.copyOf(rightChannelIPs, rightChannelIPs.length);
+    	this.rightChannelPorts = Arrays.copyOf(rightChannelPorts,  rightChannelPorts.length);
+    }
+	
+	public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+
     }
 }
